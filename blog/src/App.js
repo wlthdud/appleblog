ac843e95ccc0a65370f8b417ed792517170eb3a7
wlthdud <thdudzi@naver.com>
2023-09-15 22:22:42 +0900

    3. 버튼 추가, state 변경법

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,7 +5,8 @@ import { useState } from 'react';
 function App() {
 
 let post = '강남 우동 맛집';
-let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);  
+let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학','여자 코트 추천']);  
+let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -13,7 +14,8 @@ let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집','�
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4 onClick={()=>{글제목변경()}}>{ 글제목[0] } 
+        <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
