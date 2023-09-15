0038c8dc51c251a15be7d5a3ac543fe872e37d97
wlthdud <thdudzi@naver.com>
2023-09-15 21:58:43 +0900

    2. state 사용법

---
+++ blog/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+let post = '강남 우동 맛집';
+let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);  
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
