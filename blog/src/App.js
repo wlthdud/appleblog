92224a3c91fa98cebb237c0197de4fa2bbec44cd
wlthdud <thdudzi@naver.com>
2023-09-16 22:47:08 +0900

    6,7. 모달창, map 함수 쓰는법

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,7 +6,8 @@ function App() {
 
 let post = '강남 우동 맛집';
 let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);  
-let [따봉, 따봉변경] = useState(0);
+let [따봉, 따봉변경] = useState([0,0,0]);
+let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -26,7 +27,7 @@ let [따봉, 따봉변경] = useState(0);
       글제목변경(copy);
     }}>글 수정</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{ 글제목[0] } 
         <span onClick={()=>{ 따봉변경(따봉+1); }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
@@ -36,11 +37,31 @@ let [따봉, 따봉변경] = useState(0);
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
 
-      <Modal></Modal>
+      {
+        글제목.map(function(a,i){
+          return (
+            <div className="list" key={i}>
+              <h4>{ 글제목[i] }
+                <span onClick={()=>{ 
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy) 
+                }}>👍</span> {따봉[i]}
+              </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal == true ? <Modal/> : null
+      }
+
     </div>
   );
 }
