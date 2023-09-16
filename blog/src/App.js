ae09ce6d497e8d91ac146aa17f39e807ebb62733
wlthdud <thdudzi@naver.com>
2023-09-16 21:21:48 +0900

    5. 컴포넌트 사용법

--- blog/src/App.js
+++ blog/src/App.js
@@ -39,8 +39,20 @@ let [따봉, 따봉변경] = useState(0);
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal(){
+	return(
+		<div className="modal">
+			<h4>제목</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
+		</div>
+	)
 }
 
 export default App;
