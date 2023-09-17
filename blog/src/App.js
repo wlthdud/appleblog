bcd62b7b3ca88c91b8972089389ff7e1b3399419
wlthdud <thdudzi@naver.com>
2023-09-17 23:23:29 +0900

    9. props를 응용한 상세페이지

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,6 +8,7 @@ let post = '강남 우동 맛집';
 let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);  
 let [따봉, 따봉변경] = useState([0,0,0]);
 let [modal, setModal] = useState(false);
+let [title,setTitle] = useState(0);
 
   return (
     <div className="App">
@@ -45,7 +46,7 @@ let [modal, setModal] = useState(false);
         글제목.map(function(a,i){
           return (
             <div className="list" key={i}>
-              <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] }
+              <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>{ 글제목[i] }
                 <span onClick={()=>{ 
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
@@ -58,8 +59,12 @@ let [modal, setModal] = useState(false);
         })
       }
 
+      {/* <button onClick={()=>{setTitle(0)}}>글제목0</button>
+      <button onClick={()=>{setTitle(1)}}>글제목1</button>
+      <button onClick={()=>{setTitle(2)}}>글제목2</button> */}
+
       {
-        modal == true ? <Modal 글제목={글제목} 글제목변경={글제목변경}/> : null
+        modal == true ? <Modal 글제목={글제목} title={title}/> : null
       }
 
     </div>
@@ -69,15 +74,15 @@ let [modal, setModal] = useState(false);
 function Modal(props){
 	return(
 		<div className="modal">
-			<h4>{props.글제목[0]}</h4>
+			<h4>{props.글제목[props.title]}</h4>
 			<p>날짜</p>
 			<p>상세내용</p>
-      <button onClick={()=>{
+      {/* <button onClick={()=>{
         let copy = [...props.글제목];
         copy[0] = '여자 코트 추천';
         props.글제목변경(copy);
       }}>
-        글수정</button>
+        글수정</button> */}
 		</div>
 	)
 }
