ac266cb1207adf33ce989730c76d2a948249b6be
wlthdud <thdudzi@naver.com>
2023-09-17 22:37:42 +0900

    8. props 사용법

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
 
 let post = '강남 우동 맛집';
-let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);  
+let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);  
 let [따봉, 따봉변경] = useState([0,0,0]);
 let [modal, setModal] = useState(false);
 
@@ -45,7 +45,7 @@ let [modal, setModal] = useState(false);
         글제목.map(function(a,i){
           return (
             <div className="list" key={i}>
-              <h4>{ 글제목[i] }
+              <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] }
                 <span onClick={()=>{ 
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
@@ -59,19 +59,25 @@ let [modal, setModal] = useState(false);
       }
 
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal 글제목={글제목} 글제목변경={글제목변경}/> : null
       }
 
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
 	return(
 		<div className="modal">
-			<h4>제목</h4>
+			<h4>{props.글제목[0]}</h4>
 			<p>날짜</p>
 			<p>상세내용</p>
+      <button onClick={()=>{
+        let copy = [...props.글제목];
+        copy[0] = '여자 코트 추천';
+        props.글제목변경(copy);
+      }}>
+        글수정</button>
 		</div>
 	)
 }
